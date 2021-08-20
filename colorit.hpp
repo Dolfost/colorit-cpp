e094170b393082f7a59309a3ad0ce92185ce001c
Dolfost <dolfost@users.noreply.github.com>
2021-08-20 15:37:03 +0300

    Update to version 3.0.14
     - Change the library name from "Ansicolors" to "Colorit".
     - Change the method of accessing items.
     - Add new styles.

---
+++ colorit.hpp
@@ -0,0 +1,267 @@
+// colorit.hpp -- colors based on ANSI Escape Sequences
+// ver: 3.0.14
+
+#ifndef COLORIT_H
+#define COLORIT_H
+
+#include <iostream>
+#include <cstring>
+
+const struct Colorit_data_ {
+	// foreground
+	const char *Black_fg           {"\e[30m"};
+	const char *Red_fg             {"\e[31m"};
+	const char *Green_fg           {"\e[32m"};
+	const char *Yellow_fg          {"\e[33m"};
+	const char *Blue_fg            {"\e[34m"};
+	const char *Magneta_fg         {"\e[35m"};
+	const char *Cyan_fg            {"\e[36m"};
+	const char *White_fg           {"\e[37m"};
+	// high intensty foreground
+	const char *h_Black_fg         {"\e[90m"};
+	const char *h_Red_fg           {"\e[91m"};
+	const char *h_Green_fg         {"\e[92m"};
+	const char *h_Yellow_fg        {"\e[93m"};
+	const char *h_Blue_fg          {"\e[94m"};
+	const char *h_Magneta_fg       {"\e[95m"};
+	const char *h_Cyan_fg          {"\e[96m"};
+	const char *h_White_fg         {"\e[97m"};
+	// background
+	const char *Black_bg           {"\e[40m"};
+	const char *Red_bg             {"\e[41m"};
+	const char *Green_bg           {"\e[42m"};
+	const char *Yellow_bg          {"\e[43m"};
+	const char *Blue_bg            {"\e[44m"};
+	const char *Magneta_bg         {"\e[45m"};
+	const char *Cyan_bg            {"\e[46m"};
+	const char *White_bg           {"\e[47m"};
+	// high intensty background
+	const char *h_Black_bg         {"\e[100m"};
+	const char *h_Red_bg           {"\e[101m"};
+	const char *h_Green_bg         {"\e[102m"};
+	const char *h_Yellow_bg        {"\e[103m"};
+	const char *h_Blue_bg          {"\e[104m"};
+	const char *h_Magneta_bg       {"\e[105m"};
+	const char *h_Cyan_bg          {"\e[106m"};
+	const char *h_White_bg         {"\e[107m"};
+	// style
+	const char *Reset              {"\e[0m"};
+	const char *Bold               {"\e[1m"};
+	const char *Dim                {"\e[2m"};
+//	const char *Faint              {"\e[2m"};
+	const char *Italic             {"\e[3m"};
+	const char *Underline          {"\e[4m"};
+	const char *Slow_Blinking      {"\e[5m"};
+	const char *Rapid_Blinking     {"\e[6m"}; //
+	const char *Invert             {"\e[7m"};
+//	const char *Reverse            {"\e[7m"};
+	const char *Hide               {"\e[8m"};
+	const char *Strike             {"\e[9m"}; //
+	const char *Primary	       {"\e[10m"};
+	const char *Double_Underline   {"\e[21m"};
+	const char *Normal_Intensity   {"\e[22m"};
+	const char *Not_Underlined     {"\e[24m"};
+	const char *Not_Blinking       {"\e[25m"};
+	const char *Not_Reversed       {"\e[27m"};
+	const char *Not_Crossed_Out    {"\e[29m"};
+	const char *Default_Background {"\e[49m"};
+	const char *Overlined          {"\e[53m"};
+	const char *Not_Overlined      {"\e[55m"};
+} Colorit_data;
+
+const struct Colorit_fore_ {
+	const char *BLACK   {Colorit_data.Black_fg};
+	const char *RED     {Colorit_data.Red_fg};
+	const char *GREEN   {Colorit_data.Green_fg};
+	const char *YELLOW  {Colorit_data.Yellow_fg};
+	const char *BLUE    {Colorit_data.Blue_fg};
+	const char *MAGNETA {Colorit_data.Magneta_fg};
+	const char *CYAN    {Colorit_data.Cyan_fg};
+	const char *WHITE   {Colorit_data.White_fg};
+	// aliases
+	const char *BLK     {Colorit_data.Black_fg};
+//	const char *RED     {Colorit_data.Red_fg};
+	const char *GRN     {Colorit_data.Green_fg};
+	const char *YEL     {Colorit_data.Yellow_fg};
+	const char *BLU     {Colorit_data.Blue_fg};
+	const char *MAG     {Colorit_data.Magneta_fg};
+	const char *CYN     {Colorit_data.Cyan_fg};
+	const char *WHT     {Colorit_data.White_fg};
+} Fore;
+
+const struct Colorit_back_ {
+	const char *BLACK   {Colorit_data.Black_bg};
+	const char *RED     {Colorit_data.Red_bg};
+	const char *GREEN   {Colorit_data.Green_bg};
+	const char *YELLOW  {Colorit_data.Yellow_bg};
+	const char *BLUE    {Colorit_data.Blue_bg};
+	const char *MAGNETA {Colorit_data.Magneta_bg};
+	const char *CYAN    {Colorit_data.Cyan_bg};
+	const char *WHITE   {Colorit_data.White_bg};
+	// aliases
+	const char *BLK     {Colorit_data.Black_bg};
+//	const char *RED     {Colorit_data.Red_bg};
+	const char *GRN     {Colorit_data.Green_bg};
+	const char *YEL     {Colorit_data.Yellow_bg};
+	const char *BLU     {Colorit_data.Blue_bg};
+	const char *MAG     {Colorit_data.Magneta_bg};
+	const char *CYN     {Colorit_data.Cyan_bg};
+	const char *WHT     {Colorit_data.White_bg};	
+} Back;
+
+const struct Colorit_bright_ {
+	struct Colorit_h_Fore {
+		const char *BLACK   {Colorit_data.h_Black_fg};
+		const char *RED     {Colorit_data.h_Red_fg};
+		const char *GREEN   {Colorit_data.h_Green_fg};
+		const char *YELLOW  {Colorit_data.h_Yellow_fg};
+		const char *BLUE    {Colorit_data.h_Blue_fg};
+		const char *MAGNETA {Colorit_data.h_Magneta_fg};
+		const char *CYAN    {Colorit_data.h_Cyan_fg};
+		const char *WHITE   {Colorit_data.h_White_fg};
+		// aliases
+		const char *BLK     {Colorit_data.h_Black_fg};
+//		const char *RED     {Colorit_data.h_Red_fg};
+		const char *GRN     {Colorit_data.h_Green_fg};
+		const char *YEL     {Colorit_data.h_Yellow_fg};
+		const char *BLU     {Colorit_data.h_Blue_fg};
+		const char *MAG     {Colorit_data.h_Magneta_fg};
+		const char *CYN     {Colorit_data.h_Cyan_fg};
+		const char *WHT     {Colorit_data.h_White_fg};
+	} Fore;
+	struct Colorit_h_Back {
+		const char *BLACK   {Colorit_data.h_Black_bg};
+		const char *RED     {Colorit_data.h_Red_bg};
+		const char *GREEN   {Colorit_data.h_Green_bg};
+		const char *YELLOW  {Colorit_data.h_Yellow_bg};
+		const char *BLUE    {Colorit_data.h_Blue_bg};
+		const char *MAGNETA {Colorit_data.h_Magneta_bg};
+		const char *CYAN    {Colorit_data.h_Cyan_bg};
+		const char *WHITE   {Colorit_data.h_White_bg};
+		// aliases
+		const char *BLK     {Colorit_data.h_Black_bg};
+//		const char *RED     {Colorit_data.h_Red_bg};
+		const char *GRN     {Colorit_data.h_Green_bg};
+		const char *YEL     {Colorit_data.h_Yellow_bg};
+		const char *BLU     {Colorit_data.h_Blue_bg};
+		const char *MAG     {Colorit_data.h_Magneta_bg};
+		const char *CYN     {Colorit_data.h_Cyan_bg};
+		const char *WHT     {Colorit_data.h_White_bg};
+	} Back;
+} Bright;
+
+const struct Colorit_style_ {
+	const char *RESET            {Colorit_data.Reset};
+	const char *BOLD             {Colorit_data.Bold};
+	const char *DIM              {Colorit_data.Dim};
+	const char *FAINT            {Colorit_data.Dim};
+	const char *ITALIC           {Colorit_data.Italic};
+	const char *UNDERLINE        {Colorit_data.Underline};
+	const char *BLINK	     {Colorit_data.Slow_Blinking};
+	const char *SLOW_BLINK       {Colorit_data.Slow_Blinking};
+	const char *RAPID_BLINK      {Colorit_data.Rapid_Blinking};
+	const char *INVERT           {Colorit_data.Invert};
+	const char *REVERSE          {Colorit_data.Invert};
+	const char *HIDE             {Colorit_data.Hide};
+	const char *STRIKE           {Colorit_data.Strike};
+	const char *PRIMARY	     {Colorit_data.Primary};
+	const char *DBL_UNDERLINE    {Colorit_data.Double_Underline};
+	const char *DEF_INTENSITY    {Colorit_data.Normal_Intensity};
+	const char *NOT_UNDERLINED   {Colorit_data.Not_Underlined};
+	const char *NOT_BLINK	     {Colorit_data.Not_Blinking};
+	const char *NOT_REVERSED     {Colorit_data.Not_Reversed};
+	const char *NOT_INVERSED     {Colorit_data.Not_Reversed};
+	const char *NOT_CROSSED_OUT  {Colorit_data.Not_Crossed_Out};
+	const char *DEF_BACKGROUND   {Colorit_data.Default_Background};
+	const char *OVERLINED	     {Colorit_data.Overlined};
+	const char *NOT_OVERLINED    {Colorit_data.Not_Overlined};
+	// aliases
+	const char *RST              {Colorit_data.Reset};
+	const char *BLD              {Colorit_data.Bold};
+//	const char *DIM              {Colorit_data.Dim};
+	const char *FNT              {Colorit_data.Dim};
+	const char *ITLC             {Colorit_data.Italic};
+	const char *UNDRLN           {Colorit_data.Underline};
+	const char *BLNK	     {Colorit_data.Slow_Blinking};
+	const char *SLW_BLNK         {Colorit_data.Slow_Blinking};
+	const char *RPD_BLNK	     {Colorit_data.Rapid_Blinking};
+	const char *INVRT            {Colorit_data.Invert};
+	const char *RVRS             {Colorit_data.Invert};
+//	const char *HIDE             {Colorit_data.Hide};
+	const char *STRK             {Colorit_data.Strike};
+	const char *PRMR	     {Colorit_data.Primary};
+	const char *DBL_UNDRLN       {Colorit_data.Double_Underline};
+	const char *DEF_INTNST       {Colorit_data.Normal_Intensity};
+	const char *NOT_UNDRLND      {Colorit_data.Not_Underlined};
+	const char *NOT_BLNK	     {Colorit_data.Not_Blinking};
+	const char *NOT_RVRSD        {Colorit_data.Not_Reversed};
+	const char *NOT_INVRSD       {Colorit_data.Not_Reversed};
+	const char *NOT_CRSSD_OUT    {Colorit_data.Not_Crossed_Out};
+	const char *DEF_BKGRND       {Colorit_data.Default_Background};
+	const char *OVRLND	     {Colorit_data.Overlined};
+	const char *NOT_OVRLND       {Colorit_data.Not_Overlined};
+} Style;
+
+
+void setFore(const short arg) {
+	char ansicode[12] = "\e[38;5;";
+	char code[4];
+	snprintf(code, 4, "%d", arg);
+	strcat(ansicode, code);
+	strcat(ansicode, "m");
+	std::cout << ansicode; }
+
+void setBack(const short arg) {
+	char ansicode[12] = "\e[48;5;";
+	char code[4];
+	snprintf(code, 4, "%d", arg);
+	strcat(ansicode, code);
+	strcat(ansicode, "m");
+	std::cout << ansicode; }
+
+
+void setFore(short r, short g, short b) {
+	char red[4];
+	char green[4];
+	char blue[4];
+
+	snprintf(red, 4, "%d", r);
+	snprintf(green, 4, "%d", g);
+	snprintf(blue, 4, "%d", b);
+
+	char ansicode[20] = "\e[38;2;";
+
+	strcat(ansicode, red);
+	strcat(ansicode, ";");
+	strcat(ansicode, green);
+	strcat(ansicode, ";");
+	strcat(ansicode, blue);
+	strcat(ansicode, "m");
+
+	std::cout << ansicode; }
+
+void setBack(short r, short g, short b) {
+	char red[4];
+	char green[4];
+	char blue[4];
+
+	snprintf(red, 4, "%d", r);
+	snprintf(green, 4, "%d", g);
+	snprintf(blue, 4, "%d", b);
+
+	char ansicode[20] = "\e[48;2;{";
+	strcat(ansicode, red);
+	strcat(ansicode, ";");
+	strcat(ansicode, green);
+	strcat(ansicode, ";");
+	strcat(ansicode, blue);
+	strcat(ansicode, "m");
+
+	std::cout << ansicode;
+}
+
+#endif
+
+// last edit: 20/08/2021
+// developed by https://github.com/Dolfost
+// license : https://github.com/Dolfost/colorit-cpp/blob/main/COPYING
--- readme/run/demo.cpp
+++ readme/run/demo.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-#include "../../ansicolors.hpp"
+#include "../../colorit.hpp"
 
 int main(void) {
-	std::cout << "Colors:\n"
-	             BLK "Black \t" RED "Red \t\t" GREEN "Green \t" YEL "Yellow \n"
-	             BLU "Blue \t" MAG "Magneta \t" CYN "Cyan \t" WHT "White" sRST "\n\n"
-		     "High intensty colors:\n"
-	             BLKh "Black \t" REDh "Red \t\t" GREENh "Green \t" YELh "Yellow \n"
-	             BLU "Blue \t" MAGh "Magneta \t" CYNh "Cyan \t" WHTh "White" sRST "\n\n"
+	std::cout << "Colors:\n" <<
+	             Fore.BLK<< "Black \t" <<Fore.RED<< "Red \t\t" <<Fore.GRN<< "Green \t" <<Fore.YEL<< "Yellow \n"
+	             <<Fore.BLU<< "Blue \t" <<Fore.MAG<< "Magneta \t" <<Fore.CYN<< "Cyan \t" <<Fore.WHT<< "White" <<Style.RST<< "\n\n"
+		     << "Bright Colors:\n" <<
+	             Bright.Fore.BLK<< "Black \t" <<Bright.Fore.RED<< "Red \t\t" <<Bright.Fore.GRN<< "Green \t" <<Bright.Fore.YEL<< "Yellow \n"
+	             <<Bright.Fore.BLU<< "Blue \t" <<Bright.Fore.MAG<< "Magneta \t" <<Bright.Fore.CYN<< "Cyan \t" <<Bright.Fore.WHT<< "White" <<Style.RST<< "\n\n"
                      "Backgrounds:\n"
-	             bBLK "Black" sRST " \t" bRED "Red" sRST " \t\t" bGREEN "Green" sRST " \t" bYEL "Yellow" sRST "\n"
-	             bBLU "Blue" sRST " \t" bMAG "Magneta" sRST " \t" bCYN "Cyan" sRST " \t" bWHT "White" sRST "\n\n"
-		     "High intensty backgrounds:\n"
-	             bBLKh "Black" sRST " \t" bREDh "Red" sRST " \t\t" bGREENh "Green" sRST " \t" bYELh "Yellow" sRST "\n"
-	             bBLU "Blue" sRST " \t" bMAGh "Magneta" sRST " \t" bCYNh "Cyan" sRST " \t" bWHTh "White" sRST "\n\n"
-
+	             <<Back.BLK<< "Black" <<Style.RST<< " \t" <<Back.RED<< "Red" <<Style.RST<< " \t\t" <<Back.GREEN<< "Green" <<Style.RST<< " \t" <<Back.YEL<< "Yellow" <<Style.RST<< "\n"
+	             <<Back.BLU<< "Blue" <<Style.RST<< " \t" <<Back.MAG<< "Magneta" <<Style.RST<< " \t" <<Back.CYN<< "Cyan" <<Style.RST<< " \t" <<Back.WHT<< "White" <<Style.RST<< "\n\n"
+		     "Bright backgrounds:\n"
+	             <<Bright.Back.BLK<< "Black" <<Style.RST<< " \t" <<Bright.Back.RED<< "Red" <<Style.RST<< " \t\t" <<Bright.Back.GREEN<< "Green" <<Style.RST<< " \t" <<Bright.Back.YEL<< "Yellow" <<Style.RST<< "\n"
+	             <<Bright.Back.BLU<< "Blue" <<Style.RST<< " \t" <<Bright.Back.MAG<< "Magneta" <<Style.RST<< " \t" <<Bright.Back.CYN<< "Cyan" <<Style.RST<< " \t" <<Bright.Back.WHT<< "White" <<Style.RST<< "\n\n"
 		    "Modes:\n"
-		     mBOLD "Bold \t\t" sRST mDM "Dim \t" sRST mFAINT "Faint \t\t" sRST mITALIC "Italic \t\t" sRST mUNDERLINE "Underline" sRST "\n"
-		     mBLINKING "Blinking \t" sRST mINVERT "Invert \t" mREVERSE "Reverse \t" sRST mINVSBL "Invisible \t" sRST mSTRIKETHROUGH "Strikethrough" sRST "\n"
-		     "                 \t\t        ^   ^   ^\n"
-		     "                 \t\t        Invisible\n";
+		     <<Style.BOLD<< "Bold \t\t\t" <<Style.RST<< Style.DIM<< "Dim/faint \t" <<Style.RST<< Style.ITALIC << "Italic \t\t" <<Style.RST<< Style.UNDERLINE << "Underline" <<Style.RST<< "\n"
+		     << Style.DBL_UNDRLN << "Double Underline" << Style.RST << "\t" << Style.OVERLINED << "Overlined" << Style.RST << "\t" << Style.BLINK<< "Blinking \t" <<Style.RST<<Style.INVERT<< "Invert/Reverse \t" <<Style.RST<< "\n" 
+		     <<Style.HIDE<< "Invisible \t\t" <<Style.RST<<Style.STRIKE << "Strikethrough" << Style.RST << "\n"
+		     "^   ^   ^\n"
+		     "Invisible\n";
 	return 0;
 }
